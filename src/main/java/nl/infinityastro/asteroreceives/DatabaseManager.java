5109f41cf7001324e3b9f864587543d5b1457615
DevRune <rune.haest@gmail.com>
2024-07-27 17:44:19 +0200

    Added MariaDB support

--- src/main/java/nl/infinityastro/asteroreceives/DatabaseManager.java
+++ src/main/java/nl/infinityastro/asteroreceives/DatabaseManager.java
@@ -1,6 +1,10 @@
 package nl.infinityastro.asteroreceives;
 
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
